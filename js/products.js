182cf23cec2bc3fb6b3073f5f4f92db1c7c1e169
ausf-software <77770487+ausf-software@users.noreply.github.com>
2026-02-17 11:02:43 +0300

    update

--- js/products.js
+++ js/products.js
@@ -38,7 +38,7 @@ var base_product = [
 	new Product(1, "Finite-state machine minimization calculator", "dfsmmc_logo", "https://ausf-software.github.io/dfsmmc/"),
 	new Product(1, "Hamming Code Calculator", "hcc_logo", "https://ausf-software.github.io/hcc/"),
 	new Product(2, "Markov Algorithm", "hcc_logo", "https://ausf-software.github.io/markov-algorithm/"),
-	new Product(2, "Turing machine emulator", "hcc_logo", "https://ausf-software.github.io/turing-machine/"),
+	new Product(2, "Multi tape Turing machine emulator", "hcc_logo", "https://ausf-software.github.io/multitape-turing/"),
 	new Product(2, "Post machine emulator", "hcc_logo", "https://ausf-software.github.io/post-machine/"),
 ];
 setProducts();
